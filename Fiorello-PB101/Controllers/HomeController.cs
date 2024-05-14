88b902c52f74700aca46cb7cbf8cd46d771be7a8
Kamran-Muradov <kamranshmu@code.edu.az>
2024-05-14 20:45:31 +0400

    Created Expert Model

---
+++ Fiorello-PB101/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using Fiorello_PB101.Services.Interfaces;
+using Fiorello_PB101.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fiorello_PB101.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ISliderService _sliderService;
+        private readonly IBlogService _blogService;
+        private readonly IExpertService _expertService;
+
+        public HomeController(
+            ISliderService sliderService,
+            IBlogService blogService,
+            IExpertService expertService
+            )
+        {
+            _sliderService = sliderService;
+            _blogService = blogService;
+            _expertService = expertService;
+        }
+        public async Task<IActionResult> Index()
+        {
+            HomeVM model = new()
+            {
+                Sliders = await _sliderService.GetAllAsync(),
+                SliderInfo = await _sliderService.GetSliderInfoAsync(),
+                Blogs = await _blogService.GetAllAsync(),
+                Experts = await _expertService.GetAllAsync()
+            };
+
+            return View(model);
+        }
+
+    }
+}
---
+++ Fiorello-PB101/ViewModels/Expert/ExpertVM.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Fiorello_PB101.ViewModels.Expert
+{
+    public class ExpertVM
+    {
+        public string FullName { get; set; }
+        public string Position { get; set; }
+        public string Image { get; set; }
+    }
+}
